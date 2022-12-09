eefe01ef2d2e689020718503c1180b224e7f6b58
FishyG <triogaming122@gmail.com>
2022-12-09 01:26:41 -0500

    Added the part to read from the second serial port

--- Code/ControlRobotSumo/main.c
+++ Code/ControlRobotSumo/main.c
@@ -10,19 +10,15 @@
 
 #include <stdio.h>
 #include <catalina_cog.h>
+#include <catalina_serial4.h>
 
 // Définition des I/O de la carte Hydre
-#define USER_DEL 1<<27
-#define DEL_0 1<<24
-#define DEL_1 1<<25
 #define PWM_IO_0 1<<0
 #define PWM_IO_1 1<<2
-#define DEL_CHASER 0xff
-#define CP_FLIPFLOP 1<<26
-#define CLR  12
-
-#define user_sw1 1<<8  // Pour l'état de la switch user 1
-#define user_sw2 1<<10  // Pour l'état de la switch user 2
+#define CAPTEUR_SOL_0 1 << 4
+#define CAPTEUR_SOL_1 1 << 5
+#define CAPTEUR_SOL_2 1 << 6
+#define CAPTEUR_SOL_3 1 << 7
 
 void fct_pwm0(void);    // Prototype de la fonction qui contrôle le PWM 0
 void fct_pwm1(void);    // Prototype de la fonction qui contrôle le PWM 1
@@ -42,8 +38,6 @@ void main(int argc, char *argv[])
     unsigned long stack_d01[50]; // Stack pour le cog qui exécutera fct_del01
     int cog_pwm0 = -1;    // No associé au cog qui exécutera fct_userdel
     int cog_pwm1 = -1;   // No associé au cog qui exécutera fct_del01
-    int sw1_state = 0;  // Etat de la switch 1
-    int sw2_state = 0;  // Etat de la switch 2
 
 
     // Départ des cogs pour le contrôle de USER_DEL, DEL_0 et DEL_1
@@ -55,27 +49,45 @@ void main(int argc, char *argv[])
 
     fct_startsound();
 
+    _dira(CAPTEUR_SOL_0 | CAPTEUR_SOL_1 | CAPTEUR_SOL_2 | CAPTEUR_SOL_3, 0);
+
     while(1)
     {
-        //sw1_state = (_ina() & user_sw1);    // Lecture de l'état de la switch 1
-        //sw2_state = (_ina() & user_sw2);    // Lecture de l'état de la switch 2
 
-        speed0 = 30;
-        while(1)
+        switch(s4_rxcheck(1))
         {
-            if(speed1 < 20)
-                speed1 ++;
-            else
-            {
-                speed1 = 0;
-                if(sens0)
-                    sens0 = 0;
-                else
-                    sens0 = 1;
-            }
-
-            _waitcnt(_clockfreq()/10+_cnt()); // Délai 1/10 sec xD
+            case 'q':
+                if(speed0 <= 90)
+                speed0 += 10;
+                break;
+            case 'a':
+                if(speed0 >= 10)
+                speed0 -= 10;
+                break;
+            case 'p':
+                if(speed1 <= 90)
+                speed1 += 10;
+                break;
+            case 'l':
+                if(speed1 >= 10)
+                speed1 -= 10;
+                break;
+            //default:
+                // code block
         }
+        s4_rxflush(1);
+        /*if(speed1 < 20)
+            speed1 ++;
+        else
+        {
+            speed1 = 0;
+            if(sens0)
+                sens0 = 0;
+            else
+                sens0 = 1;
+        }*/
+
+        _waitcnt(_clockfreq()/10+_cnt()); // Délai 1/10 sec xD
     }
 }
 
@@ -91,22 +103,25 @@ void fct_pwm0(void)
 
     while(1)
     {
-        sens0temp = sens0;
-        speed0temp = speed0;
-
-        _outa(PWM_IO_0 << sens0temp ^ 1, 0);
-
-        while(i < speed0temp)
+        if(speed0)
         {
+            sens0temp = sens0;
+            speed0temp = speed0;
+            _outa(PWM_IO_0 << sens0temp ^ 1, 0);
             _outa(PWM_IO_0 << sens0temp, PWM_IO_0 << sens0temp); // PWM haut
-            i++;
-        }
-        while(i < 100)
-        {
+            while(i < speed0temp)
+            {
+                //_outa(PWM_IO_0 << sens0temp, PWM_IO_0 << sens0temp); // PWM haut
+                i++;
+            }
             _outa(PWM_IO_0 << sens0temp, 0); // PWM bas
-            i++;
+            while(i < 100)
+            {
+                //_outa(PWM_IO_0 << sens0temp, 0); // PWM bas
+                i++;
+            }
+            i = 0;
         }
-        i = 0;
     }
 }
 
@@ -122,24 +137,25 @@ void fct_pwm1(void)
 
     while(1)
     {
-        sens1temp = sens1;
-        speed1temp = speed1;
-
-        _outa(PWM_IO_1 << sens1temp ^ 1, 0);
-
-        _outa(PWM_IO_1 << sens1temp, PWM_IO_1 << sens1temp); // PWM haut
-        while(i < speed1temp)
+        if(speed1)
         {
-            //_outa(PWM_IO_1 << sens1temp, PWM_IO_1 << sens1temp); // PWM haut
-            i++;
+            sens1temp = sens1;
+            speed1temp = speed1;
+            _outa(PWM_IO_1 << sens1temp ^ 1, 0);
+            _outa(PWM_IO_1 << sens1temp, PWM_IO_1 << sens1temp); // PWM haut
+            while(i < speed1temp)
+            {
+                //_outa(PWM_IO_1 << sens1temp, PWM_IO_1 << sens1temp); // PWM haut
+                i++;
+            }
+            _outa(PWM_IO_1 << sens1temp, 0); // PWM bas
+            while(i < 100)
+            {
+                //_outa(PWM_IO_1 << sens1temp, 0); // PWM bas
+                i++;
+            }
+            i = 0;
         }
-        _outa(PWM_IO_1 << sens1temp, 0); // PWM bas
-        while(i < 100)
-        {
-            //_outa(PWM_IO_1 << sens1temp, 0); // PWM bas
-            i++;
-        }
-        i = 0;
     }
 }
 
